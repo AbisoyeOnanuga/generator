7e1c1abf8f7eabfe3a96de2d7cf9c600bbfb9eb1
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-04-24 15:34:56 -0400

    update API generator app

---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+
+import google.generativeai as genai
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key="YOUR_API_KEY")
+
+# Set up the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+  "response_mime_type": "application/json",
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+response = model.generate_content("The opposite of hot is")
+print(response.text)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"text": "An API for generating data"}
+
+@app.post("/generate")
+def generate_data():
+    return {"text": "data generated successfully"}
